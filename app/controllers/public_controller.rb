beff3b91d207899bedf71ab039bd1e44a48e7e14
Wilson Bilkovich <wilson@supremetyrant.com>
2011-09-02 12:59:45 -0400

    Hrm.. shows 'false'..

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,9 +1,11 @@
 require 'pp'
 class PublicController < ApplicationController
   def index
-    path = File.join(Rails.root, 'config', 'ey.yml')
+    path = Rails.root.join('config', 'ey.yml').to_s
     if File.readable?(path)
-      @config = YAML.load_file(path)
+      File.open(path) do |f|
+        @config = YAML.load(f.read)
+      end
     else
       @config = {'db_host' => 'localhost', 'RACK_ENV' => ENV['RACK_ENV']}
     end
