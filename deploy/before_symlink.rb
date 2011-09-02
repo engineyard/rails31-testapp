d912066ee5f220d14f46e22aeb82285697b49bf3
Wilson Bilkovich <wilson@supremetyrant.com>
2011-09-02 14:47:25 -0400

    Less goofy config info

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,10 @@
 yml_path = File.join(release_path, 'config', 'ey.yml')
 File.open(yml_path, 'w') do |f|
-  f.write(YAML.dump(node))
+  c = {}
+  e = node['environment']
+  c[:name] = e['name']
+  c[:stack] = e['stack']
+  c[:RACK_ENV] = e['framework_env']
+  c[:public_hostname] = e['engineyard']['instances'].detect {|h| h['public_hostname']} || `hostname`
+  f.write(YAML.dump(c))
 end
-
