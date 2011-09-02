2a3f9c405c6eb49349cde5c0b282347392d789cc
Wilson Bilkovich <wilson@supremetyrant.com>
2011-09-02 14:08:03 -0400

    I am a stupid, stupid man.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,5 @@
 yml_path = File.join(release_path, 'config', 'ey.yml')
 File.open(yml_path, 'w') do |f|
-  $stderr.puts "dumping node info: #{node.inspect}"
-  YAML.dump(node)
+  f.write(YAML.dump(node))
 end
 
